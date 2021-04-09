cffd444d0c7772b2c58a6b67800ffe865b373558
Danilo Koerber (FG-810) <danilo@easyware.com.br>
2021-04-09 13:50:29 +0200

    Update Api.java

--- src/main/java/io/easyware/platypus/api/mail/Api.java
+++ src/main/java/io/easyware/platypus/api/mail/Api.java
@@ -35,14 +35,14 @@ public class Api {
     @Produces(MediaType.APPLICATION_JSON)
     public Response webhook(WebhookMessage webhookMessage) {
 
-        System.out.println("New message from " + webhookMessage.getData().getFrom().getAddress());
-        System.out.println("Special use " + webhookMessage.getSpecialUse());
+        System.out.println("New message from in " + webhookMessage.getSpecialUse() + " " + webhookMessage.getData().getFrom().getAddress());
         if (webhookMessage.getSpecialUse() == "\\All") {
+            System.out.print("Checking still unread messages.... ");
             MessageList messages = service.get(webhookMessage.getAccount(), webhookMessage.getPath());
             long unreadMessages = messages.getMessages().stream().filter(message -> message.isUnseen()).count();
-            System.out.println(unreadMessages + " unread messages.");
+            System.out.println(unreadMessages + " unread messages found.");
         }
-        
+
         return Response.ok().build();
     }
 }

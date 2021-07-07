e8c80f7691090fbead08544e56877e987f79e626
Danilo Koerber <danilo@easyware.com.br>
2021-07-07 20:52:54 +0200

    Update UserRepresentationAttributes.java

--- src/main/java/io/easyware/platypus/api/users/objects/UserRepresentationAttributes.java
+++ src/main/java/io/easyware/platypus/api/users/objects/UserRepresentationAttributes.java
@@ -21,6 +21,9 @@ public class UserRepresentationAttributes {
     public void setCountry(List<String> country) {
         this.country = country.get(0);
     }
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
     public String getBirthdate() {
         return birthdate;
@@ -28,6 +31,9 @@ public class UserRepresentationAttributes {
 
     public void setBirthdate(List<String> birthdate) {
         this.birthdate = birthdate.get(0);
+    }
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
     }
 
     public String getStreet() {
@@ -37,6 +43,9 @@ public class UserRepresentationAttributes {
     public void setStreet(List<String> street) {
         this.street = street.get(0);
     }
+    public void setStreet(String street) {
+        this.street = street;
+    }
 
     public String getPostalCode() {
         return postalCode;
@@ -44,6 +53,9 @@ public class UserRepresentationAttributes {
 
     public void setPostalCode(List<String> postalCode) {
         this.postalCode = postalCode.get(0);
+    }
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 
     public String getLocality() {
@@ -53,6 +65,9 @@ public class UserRepresentationAttributes {
     public void setLocality(List<String> locality) {
         this.locality = locality.get(0);
     }
+    public void setLocality(String locality) {
+        this.locality = locality;
+    }
 
     public String getLocale() {
         return locale;
@@ -61,6 +76,9 @@ public class UserRepresentationAttributes {
     public void setLocale(List<String> locale) {
         this.locale = locale.get(0);
     }
+    public void setLocale(String locale) {
+        this.locale = locale;
+    }
 
     public String getPicture() {
         return picture;
@@ -68,5 +86,8 @@ public class UserRepresentationAttributes {
 
     public void setPicture(List<String> picture) {
         this.picture = picture.get(0);
+    }
+    public void setPicture(String picture) {
+        this.picture = picture;
     }
 }
